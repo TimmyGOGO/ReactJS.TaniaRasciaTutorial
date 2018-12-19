fd1b82b20844cbfaff820422284dec86c05ef4d8
TimmyGOGO <temich941@mail.ru>
2018-12-19 17:31:40 +0300

    Updates

---
+++ precache-manifest.bf7e432e41f902a41c335863f4bc2699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "0dde9d50b02f4a659854",
+    "url": "/react-tutorial/static/js/main.0dde9d50.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-tutorial/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "0dde9d50b02f4a659854",
+    "url": "/react-tutorial/static/css/main.af0126d7.chunk.css"
+  },
+  {
+    "revision": "4f61408c46b9aca26128dd35ea00b95a",
+    "url": "/react-tutorial/index.html"
+  }
+];
