53c6d9db6576db78ce58ed09081cab17a8dccc9a
TimmyGOGO <temich941@mail.ru>
2018-12-20 11:32:47 +0300

    Updates

--- precache-manifest.8cd4799d285756a639d659a7caec1e58.js
+++ precache-manifest.8cd4799d285756a639d659a7caec1e58.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/ReactJS.TaniaRasciaTutorial/static/js/runtime~main.9daa5a46.js"
   },
   {
-    "revision": "65c6a65f9bba71eac2c3",
-    "url": "/ReactJS.TaniaRasciaTutorial/static/js/main.65c6a65f.chunk.js"
+    "revision": "8a4a73230c8e2f67b654",
+    "url": "/ReactJS.TaniaRasciaTutorial/static/js/main.8a4a7323.chunk.js"
   },
   {
     "revision": "2818dbc80ba3c939eefa",
     "url": "/ReactJS.TaniaRasciaTutorial/static/js/1.2818dbc8.chunk.js"
   },
   {
-    "revision": "65c6a65f9bba71eac2c3",
+    "revision": "8a4a73230c8e2f67b654",
     "url": "/ReactJS.TaniaRasciaTutorial/static/css/main.0c3a480d.chunk.css"
   },
   {
-    "revision": "ad70b095390c5087ab0407ae0822973f",
+    "revision": "d6aea368f6601351724cb31079789760",
     "url": "/ReactJS.TaniaRasciaTutorial/index.html"
   }
 ];
