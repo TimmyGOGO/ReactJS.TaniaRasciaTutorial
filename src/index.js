9e55c1e32f3991da425289068b4e3473b801ce8e
TimmyGOGO <temich941@mail.ru>
2018-12-20 12:19:30 +0300

    Finally, deployment works! You need to specify the branch for github.pages!

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './index.css';
 import App from './components/App';
 // import App from './components/Api';
 
-ReactDOM.render(<div>HELLO WORLD</div>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
